e9fd047b2d9812d3417024bc1bdbe969f0f439e7
Calvin Reed <me@calvinreed.com>
2024-07-17 21:32:42 -0400

    Add timestamp to demo

--- CReed.TimestampGuid.Demo/Main.cs
+++ CReed.TimestampGuid.Demo/Main.cs
@@ -3,5 +3,7 @@
 for (var i = 0; i < 10; i++)
 {
     await Task.Delay(100);
-    Console.WriteLine(TimestampGuid.NextGuid());
+    var guid = TimestampGuid.NextGuid();
+    var timestamp = TimestampGuid.GetTimestamp(guid).ToLocalTime();
+    Console.WriteLine($"{guid} {timestamp:O}");
 }
