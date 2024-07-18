9bdb38e82ac075c48c3cb31f63d25a4a2ba9c7fc
Calvin Reed <me@calvinreed.com>
2024-07-18 06:14:01 -0400

    New test. Order by fundamental-ness.

--- CReed.UUIDv7.Test/TimestampGuidMust.cs
+++ CReed.UUIDv7.Test/TimestampGuidMust.cs
@@ -5,25 +5,6 @@ namespace CReed.UUIDv7.Test;
 public class TimestampGuidMust
 {
     [Fact]
-    public void BeMonotonic()
-    {
-        var prev = TimestampGuid.NextGuid();
-        for (var i = 0; i < 1_000_000; i++)
-        {
-            var current = TimestampGuid.NextGuid();
-            Assert.True(prev < current);
-        }
-    }
-
-    [Fact]
-    public void HaveCorrectTimestamp()
-    {
-        var guid = TimestampGuid.NextGuid();
-        var timestamp = TimestampGuid.GetTimestamp(guid);
-        Assert.Equal(DateTimeOffset.UtcNow, timestamp, TimeSpan.FromMilliseconds(3));
-    }
-
-    [Fact]
     public void HaveCorrectVersion()
     {
         var guid = TimestampGuid.NextGuid();
@@ -43,5 +24,34 @@ public class TimestampGuidMust
         var back = BinaryPrimitives.ReadInt64BigEndian(span[8..]);
         var variant = back >>> 62;
         Assert.Equal(0b10, variant);
+    }
+
+    [Fact]
+    public void HaveCorrectTimestamp()
+    {
+        var now = DateTimeOffset.UtcNow;
+        var guid = TimestampGuid.NextGuid();
+        var timestamp = TimestampGuid.GetTimestamp(guid);
+        Assert.Equal(now, timestamp, TimeSpan.FromMilliseconds(1));
+    }
+
+    [Fact]
+    public void RejectInvalidGuids()
+    {
+        var guid = Guid.NewGuid();
+        Assert.False(TimestampGuid.TryGetTimestamp(guid, out _));
+        Assert.Null(TimestampGuid.TryGetTimestamp(guid));
+        Assert.Throws<ArgumentException>(() => TimestampGuid.GetTimestamp(guid));
+    }
+
+    [Fact]
+    public void BeMonotonic()
+    {
+        var prev = TimestampGuid.NextGuid();
+        for (var i = 0; i < 1_000_000; i++)
+        {
+            var current = TimestampGuid.NextGuid();
+            Assert.True(prev < current);
+        }
     }
 }
