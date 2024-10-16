959ea8bf20153d09304fc2ac287b19ae238c54b4
Calvin Reed <me@calvinreed.com>
2024-10-16 17:33:22 -0400

    Implement UUID v3

---
+++ CReed.GuidVariant/GuidV3.cs
@@ -0,0 +1,23 @@
+using System.Security.Cryptography;
+
+namespace CReed;
+
+public sealed class GuidV3(Guid prefix) : HashGuid(prefix)
+{
+    protected override int Version => 0x30;
+
+    protected override void HashData(ReadOnlySpan<byte> source, Span<byte> destination)
+    {
+        MD5.HashData(source, destination);
+    }
+
+    protected override void HashData(Stream source, Span<byte> destination)
+    {
+        MD5.HashData(source, destination);
+    }
+
+    protected override ValueTask<byte[]> HashDataAsync(Stream source, CancellationToken token)
+    {
+        return MD5.HashDataAsync(source, token);
+    }
+}
