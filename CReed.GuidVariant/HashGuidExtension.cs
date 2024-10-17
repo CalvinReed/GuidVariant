3bdfc2508498def5555b48e2194526e5fa9af7c8
Calvin Reed <me@calvinreed.com>
2024-10-16 20:31:10 -0400

    Typo

--- CReed.GuidVariant/HashGuidExtension.cs
+++ CReed.GuidVariant/HashGuidExtension.cs
@@ -2,7 +2,7 @@ using System.Diagnostics.Contracts;
 
 namespace CReed;
 
-public static class HasGuidExtension
+public static class HashGuidExtension
 {
     [Pure]
     public static Guid NewGuid(this HashGuid hashGuid, Guid prefix, string? data)
